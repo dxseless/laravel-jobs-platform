157d1c9a6324bc82345e1680e8d868776415ea10
dxse <dm739165@gmail.com>
2025-01-13 12:56:52 +0300

    install laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,52 +1,20 @@
 <?php
 
-use App\Models\Employer;
-use App\Models\Job;
-use App\Models\User;
-use Illuminate\Http\Request;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
 });
 
-Route::get('/jobs', function (Request $request) {
-    $jobs = Job::query()
-        ->when($request->has('title'), function ($query) use ($request) {
-            $query->where('title', 'like', '%' . $request->title . '%');
-        })
-        ->when($request->has('location'), function ($query) use ($request) {
-            $query->where('location', 'like', '%' . $request->location . '%');
-        })
-        ->when($request->has('salary_min'), function ($query) use ($request) {
-            $query->where('salary', '>=', $request->salary_min);
-        })
-        ->when($request->has('salary_max'), function ($query) use ($request) {
-            $query->where('salary', '<=', $request->salary_max);
-        })
-        ->paginate(10);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-    return view('jobs', ['jobs' => $jobs]);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/job/{job}', function (Job $job) {
-    return view('job', ['job' => $job]);
-});
-
-Route::get('/contact', function () {
-    return view('contact');
-});
-
-Route::get('/users', function () {
-    return view('users', ['users' => User::paginate(3)]);
-});
-
-Route::get('/employers', function () {
-    return view('employers', ['employers' => Employer::paginate(3)]);
-});
-
-Route::get('/employers/{employer}', function (Employer $employer) {
-    $employer->load('jobs');
-    return view('employer', ['employer' => $employer]);
-});
+require __DIR__.'/auth.php';
