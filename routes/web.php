c3a892c5a5a77931ba46d0962ab73b1ee82bda77
dxseless <dm739165@gmail.com>
2025-02-28 11:53:06 +0300

    routes refactoring

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionController;
 use App\Models\Employer;
-use App\Models\Job;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
@@ -12,29 +11,19 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::prefix('jobs')->group(function () {
-    Route::get('/', [JobController::class, 'index'])->name('jobs.index');
-    Route::get('/{job}', [JobController::class, 'show'])->name('jobs.show')->whereNumber('job');
+Route::get('/register', [RegisteredUserController::class, 'create'])->name('register');
+Route::post('/register', [RegisteredUserController::class, 'store']);
+Route::get('/login', [SessionController::class, 'create'])->name('login');
+Route::post('/login', [SessionController::class, 'store']);
+Route::post('/logout', [SessionController::class, 'destroy'])->name('logout');
 
+Route::resource('jobs', JobController::class)->except(['edit', 'update']);
+
+Route::prefix('jobs')->group(function () {
     Route::post('/{job}/like', [JobController::class, 'like'])->name('jobs.like');
     Route::delete('/{job}/unlike', [JobController::class, 'unlike'])->name('jobs.unlike'); 
     Route::post('/{job}/favorite', [JobController::class, 'addToFavorites'])->name('jobs.favorite');
     Route::delete('/{job}/unfavorite', [JobController::class, 'removeFromFavorites'])->name('jobs.unfavorite'); 
-    
-    Route::get('/create', function () {
-        return view('jobs.create');
-    });
-    Route::post('/create', function () {
-        $attributes = request()->validate([
-            'title' => ['required', 'min:5'],
-            'salary' => ['required', 'numeric'],
-            'location' => 'required',
-        ]);
-
-        Job::create($attributes);
-
-        return redirect('/jobs');
-    });
 });
 
 Route::get('/contact', function () {
@@ -42,7 +31,8 @@ Route::get('/contact', function () {
 });
 
 Route::get('/users', function () {
-    return view('users', ['users' => User::paginate(3)]);
+    $users = User::latest()->paginate(3); 
+    return view('users', ['users' => $users]);
 });
 
 Route::prefix('employers')->group(function () {
@@ -54,10 +44,3 @@ Route::prefix('employers')->group(function () {
         return view('employer', ['employer' => $employer]);
     });
 });
-
-Route::get('/register', [RegisteredUserController::class, 'create']);
-Route::post('/register', [RegisteredUserController::class, 'store']);
-
-Route::get('/login', [SessionController::class, 'create']);
-Route::post('/login', [SessionController::class, 'store']);
-Route::post('/logout', [SessionController::class, 'destroy']);
