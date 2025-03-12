39b350d75096ae477a8a618d4e5f8b4928baabab
dxseless <dm739165@gmail.com>
2025-03-13 00:23:42 +0300

    refactored JobController

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -5,47 +5,41 @@ namespace App\Http\Controllers;
 use App\Mail\JobPosted;
 use App\Models\Employer;
 use App\Models\Job;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Mail;
-use App\Http\Requests\JobRequest; // Импортируем класс запроса
+use App\Http\Requests\JobRequest;
 
 class JobController extends Controller
 {
     public function index()
     {
-        $jobs = Job::query()->orderBy('id', 'DESC')->paginate(5);
-
-        return view('jobs.index', ['jobs' => $jobs]);
+        $jobs = Job::latest()->paginate(5);
+        return view('jobs.index', compact('jobs'));
     }   
     
     public function show(Job $job)
     {
-        return view('jobs.show', ['job' => $job]);
+        return view('jobs.show', compact('job'));
     }
 
     public function edit(Job $job)
     {
-        return view('jobs.edit', ['job' => $job]);
+        return view('jobs.edit', compact('job'));
     }
 
     public function destroy(Job $job)
     {
         Gate::authorize('edit', $job);
-        
         $job->delete();
-        
-        return redirect('/jobs');
+        return redirect('/jobs')->with('success', 'Работа успешно удалена.');
     }
 
     public function update(JobRequest $request, Job $job)
     {
         Gate::authorize('edit', $job);
-
         $job->update($request->validated()); 
-
-        return redirect('/jobs')->with('success', 'Job updated successfully.');
+        return redirect('/jobs')->with('success', 'Работа успешно обновлена.');
     }
 
     public function store(JobRequest $request) 
@@ -65,19 +59,16 @@ class JobController extends Controller
             'employer_id' => $employer->id,
         ]));
 
-        Mail::to($job->employer->user->email)->queue(
-            new JobPosted($job)
-        );
+        Mail::to($job->employer->user->email)->queue(new JobPosted($job));
 
-        return redirect('/jobs');
+        return redirect('/jobs')->with('success', 'Работа успешно размещена.');
     }
 
     public function create()
     {
         if (Auth::guest()) {
-            return redirect('/login');
-        };
-
+            return redirect()->route('login');
+        }
         return view('jobs.create');
     }
 }
